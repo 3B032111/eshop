3486d76ab0bd86e46f2cbc8f96bd5a1fe49f4202
3B032111 <3b032111@gm.student.ncut.edu.tw>
2023-11-28 03:06:17 +0800

    git 增加三個 name email password and 新增remember_token

--- database/migrations/2023_11_27_182238_create_staff_table.php
+++ database/migrations/2023_11_27_182238_create_staff_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('staff', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->rememberToken(); // 增加 remember_token 欄位
             $table->timestamps();
         });
     }
