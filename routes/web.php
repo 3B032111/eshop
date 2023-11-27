16197a36b0d1e3c5e1f819fa79aa99fa56daaa0e
3B032111 <3b032111@gm.student.ncut.edu.tw>
2023-11-28 02:26:10 +0800

    ans blog/lara_0901 vs eshop route比較 個別敘述說明

--- routes/web.php
+++ routes/web.php
@@ -48,3 +48,50 @@ URL: /Products/{Product}/edit
 HTTP 方法: GET、HEAD
 控制器&方法: ProductController@edit
  */
+
+
+
+
+/*
+ Blog / lara_0901 vs eshop 路由比較
+ Blog admin開頭route URL
+  Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', [AdminHomeController::class, 'index'])->name("home.index");
+    Route::get('posts', [AdminPostsController::class, 'index'])->name("posts.index");
+    Route::get('posts/create', [AdminPostsController::class, 'create'])->name("posts.create");
+    Route::post('posts', [AdminPostsController::class, 'store'])->name("posts.store");
+    Route::get('posts/{post}/edit', [AdminPostsController::class, 'edit'])->name("posts.edit");
+    Route::patch('posts/{post}', [AdminPostsController::class, 'update'])->name("posts.update");
+    Route::delete('posts/{post}', [AdminPostsController::class, 'destroy'])->name("posts.destroy");
+});
+    eshop route
+     Route::resource('Products', ProductController::class);
+兩者區別
+Blog and lara_0901
+使用前綴方法(prefix)可以為組中的每個路由新增特定的 URI 前綴，用來權限管理
+eshop
+直接透過URI進入
+7個Products路由作用
+products.index
+index
+顯示所有產品的列表，用於查看所有可用產品。
+products.show
+show
+顯示特定產品的詳細信息，用於查看單個產品的詳情。
+products.create
+create
+顯示用於創建新產品的表單，用戶可以在這裡輸入必要的信息。
+products.store
+store
+處理創建新產品的請求，將新產品的資訊存儲到資料庫中。
+products.edit
+edit
+顯示用於編輯現有產品訊息的表單，用戶可以在這裡更新產品的內容。
+products.update
+update
+處理編輯現有產品資訊的請求，將更新後的資訊保存到資料庫中。
+products.destroy
+destroy
+處理刪除特定產品的請求，從資料庫中刪除相對應的產品記錄。
+ *
+ */
