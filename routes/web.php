2a886781430997059b6ede7097e69dfda49f26d3
3B032111 <3b032111@gm.student.ncut.edu.tw>
2023-11-28 02:25:21 +0800

    ans URL HTTP方法 與所串接的控制器

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,34 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('Products',ProductController::class);
+/*
+Products.index
+URL: /Products
+HTTP 方法: GET、HEAD
+控制器&方法: ProductController@index
+Products.store
+URL: /Products
+HTTP 方法: POST
+控制器&方法: ProductController@store
+Products.create
+URL: /Products/create
+HTTP 方法: GET、HEAD
+控制器&方法: ProductController@create
+Products.show
+URL: /Products/{Product}
+HTTP 方法: GET、HEAD
+控制器&方法: ProductController@show
+Products.update
+URL: /Products/{Product}
+HTTP 方法: PUT、PATCH
+控制器&方法: ProductController@update
+Products.destroy
+URL: /Products/{Product}
+HTTP 方法: DELETE
+控制器&方法: ProductController@destroy
+Products.edit
+URL: /Products/{Product}/edit
+HTTP 方法: GET、HEAD
+控制器&方法: ProductController@edit
+ */
